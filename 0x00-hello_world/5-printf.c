5901eef6515da23a59843f0882358fc2b0458e7a
doscod <yeliandossa.9@gmail.com>
2022-04-07 15:11:43 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,16 @@
 
 
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return : Always 0 (Success)
  */
+
 int main(void)
+
 {
+
      printf("with proper grammar, but the outcome is a piece of art,\n");
      return(0);
+
 }
