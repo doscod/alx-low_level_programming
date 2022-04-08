884c31fd34031d716683250d88ac34c54456f5f2
doscod <yeliandossa.9@gmail.com>
2022-04-08 15:26:31 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Return: Always (0) (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf ("%d is negative \n", n);
+		printf("%d is negative \n", n);
 	}
 	else
 	{
-		printf ("%d is zero \n", n);
+		printf("%d is zero \n", n);
 	}
 	return (0);
 }
