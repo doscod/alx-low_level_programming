0965b7c8fcc6d38f5dcf4acec296fcd319e380ec
doscod <yeliandossa.9@gmail.com>
2022-04-08 18:30:37 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - task9
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+		{
+			letsgo;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+
+}
