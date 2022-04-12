9d623bb7134ee6296e3461e870821b8c29957a33
doscod <yeliandossa.9@gmail.com>
2022-04-12 15:57:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return nothing 
+ */
+
+void print_alphabet(void)
+{
+
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ch++)
+	{
+		_putchar(ch);
+	}	
+	_putchar('\n');
+}
