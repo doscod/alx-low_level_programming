bfefab5717cd751afe3af0e6220fbcff7f389cc4
doscod <yeliandossa.9@gmail.com>
2022-04-12 18:55:55 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,9 +3,7 @@
 /**
  * times_table - Prints the 9 times table, starting with 0.
  */
-
 void times_table(void)
-
 {
 	int num, mult, prod;
 
@@ -27,7 +25,5 @@ void times_table(void)
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
-	
 	}
-
 }
