88abe76f6b1658a97685fef17502eaa79a7a30a8
doscod <yeliandossa.9@gmail.com>
2022-04-14 13:28:29 -0700

    main file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
-:x
-
-i
+#ifndef MAIN_H
 #define MAIN_H
 
 /**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- * used in the 0x03-more_functions_nested_loops directory.
+ *   used in the 0x03-more_functions_nested_loops directory.
  */
 
 int _putchar(char c);
