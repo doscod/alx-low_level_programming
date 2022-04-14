f515e4ded2e92c9716b4909cec3a9867aaaeadb2
doscod <yeliandossa.9@gmail.com>
2022-04-14 14:17:03 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,14 @@
  */
 void print_line(int n)
 {
-	int length;
+	int i;
 
 	if (n > 0)
 	{
-		for (length = 0; length < n; length++)
+		for (i = 0; i < n; i++)
+		{
 			_putchar('_');
+		}
 	}
-
 	_putchar('\n');
 }
