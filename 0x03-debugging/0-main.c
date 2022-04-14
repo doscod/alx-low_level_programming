d183c1eb6773556547cd250c87046f5b2481e205
doscod <yeliandossa.9@gmail.com>
2022-04-14 07:40:19 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
+
 /**
  * main - Test function for positive or negative
  *
  * Return 0
  */
-
 int main(void)
 {
-	int a = 0;
+	int i = 0;
 
-	positive_or_negative(a);
+	positive_or_negative(i);
 
 	return (0);
 }
