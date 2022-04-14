be0ca25e24ce231279f8bcc9502cd48850f6c122
doscod <yeliandossa.9@gmail.com>
2022-04-14 12:59:47 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Prototype: int _isupper(int c);
+ *
+ * Returns 1 if c is uppercase
+ *
+ * Returns 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+
