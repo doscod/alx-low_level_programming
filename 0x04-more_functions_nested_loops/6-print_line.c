7dc72d60b95ea80927c89f73605b8c46722a1165
doscod <yeliandossa.9@gmail.com>
2022-04-14 17:41:24 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,6 +4,7 @@
  * print_line - Draws a straight line using the character _.
  * @n: The number of _ characters to be printed.
  */
+
 void print_line(int n)
 {
 	int length;
@@ -11,10 +12,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (length = 0; length < n; length++)
-		{
 			_putchar('_');
-		}
 	}
-
 	_putchar('\n');
 }
