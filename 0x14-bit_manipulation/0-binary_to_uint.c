97812a54280968f03c23d7052595c2ff5eb211a6
doscod <yeliandossa.9@gmail.com>
2022-06-18 08:19:39 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,13 +7,9 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  * Otherwise - the converted number.
  */
-
 unsigned int binary_to_uint(const char *b)
-
 {
-
 	unsigned int num = 0, mult = 1;
-
 	int len;
 
 	if (b == '\0')
@@ -25,19 +21,13 @@ unsigned int binary_to_uint(const char *b)
 		len++;
 
 	for (len -= 1; len >= 0; len--)
-
 	{
-
 		if (b[len] != '0' && b[len] != '1')
 
 			return (0);
 
 		num += (b[len] - '0') * mult;
-
 		mult *= 2;
-
 	}
-
 	return (num);
-
 }
